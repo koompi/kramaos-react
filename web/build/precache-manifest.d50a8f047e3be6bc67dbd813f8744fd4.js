dbf5ad55b91df2e52fd68ae3143c64388a9e9480
Mr. Ayoung <saing.sab@gmail.com>
2019-03-26 13:52:55 +0700

    Add after Deploy

---
+++ web/build/precache-manifest.d50a8f047e3be6bc67dbd813f8744fd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ed8a82856647d5f9f3f",
+    "url": "/static/css/main.b89a357a.chunk.css"
+  },
+  {
+    "revision": "1ed8a82856647d5f9f3f",
+    "url": "/static/js/main.46b53924.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed5d7fa8a5e30b11bcf6",
+    "url": "/static/js/2.e57da2f2.chunk.js"
+  },
+  {
+    "revision": "66f0c8118ad4e10ecc74bf75e2678ae6",
+    "url": "/index.html"
+  }
+];
